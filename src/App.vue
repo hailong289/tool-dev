e718da584311fc3c7a32c02d629c40cd41e52722
Hai Long <longdh2.dev@gmail.com>
2024-03-28 13:28:12 +0700

    first

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import MonacoVue from './components/Monaco.vue';
+</script>
+
+<template>
+  <div class="menu">
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <div class="content-right">
+     <MonacoVue />
+  </div>
+</template>
+
+<style scoped>
+.menu {
+  width: 200px;
+}
+.content-right{
+  width: calc(100% - 200px);
+  height: 100vh;
+}
+</style>
---
+++ src/components/Monaco.vue
@@ -0,0 +1,47 @@
+<script>
+import { defineComponent, ref, onMounted, onUnmounted } from "vue";
+import * as monaco from 'monaco-editor'
+import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import TsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+
+// @ts-ignore
+self.MonacoEnvironment = {
+  getWorker(_, label) {
+    if (['typescript', 'javascript'].includes(label)) {
+      return new TsWorker()
+    }
+    return new EditorWorker();
+  }
+}
+
+export default defineComponent({
+  setup() {
+    const root = ref();
+    let editor;
+    onMounted(() => {
+      editor = monaco.editor.create(root.value, {
+        language: 'typescript',
+        value: `console.log("hello world"); `,
+        theme: 'vs-dark',
+      })
+    })
+    onUnmounted(() => {
+      editor.dispose()
+    })
+    return {
+      root
+    }
+  }
+});
+</script>
+<template>
+    <div id="root" ref="root">
+
+    </div>
+</template>
+<style scoped>
+#root {
+  width: 100%;
+  height: 100vh;
+}
+</style>
